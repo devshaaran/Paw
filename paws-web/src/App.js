624249656a005268cbbdbe51ec9ce978e68aeb90
jakeols <jake_ols@live.com>
2019-10-02 14:33:00 -0700

    initial map setup

--- paws-web/src/App.js
+++ paws-web/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/navbar';
+import Searchbar from './components/searchbar';
+import Mapviewer from './components/map';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default class App extends Component {
+  render(){
+    return (
+      <div className="App container">
+      <div>
+        Paws Explorer
+      </div>
+      <Searchbar />
+
+      <h3 style={{textAlign: "left"}}>Create a Dog-Walking Route</h3>
+      <Mapviewer />
+
+      <h3>Explore Your Area</h3>
+      </div>
+    )
+  }
 }
 
-export default App;
+
