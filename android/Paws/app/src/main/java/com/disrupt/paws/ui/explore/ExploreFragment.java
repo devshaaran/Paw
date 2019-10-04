ba1efeef92d1eaf070b61000174a664332e368b0
Aditya Prabhu <adityaprabhu@galo-pazs-mbp.attlocal.net>
2019-10-04 06:09:22 -0700

    Icon + minor changes

--- android/Paws/app/src/main/java/com/disrupt/paws/ui/explore/ExploreFragment.java
+++ android/Paws/app/src/main/java/com/disrupt/paws/ui/explore/ExploreFragment.java
@@ -9,28 +9,20 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 import com.disrupt.paws.R;
-import com.disrupt.paws.model.MapMarker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
-
-import static androidx.constraintlayout.widget.Constraints.TAG;
-
 
 public class ExploreFragment extends Fragment implements View.OnClickListener, OnMapReadyCallback {
 
+    private static final String TAG = ExploreFragment.class.getSimpleName();
     //private ExploreViewModel exploreViewModel;
     private MapView mMapView;
     private ImageButton btnOptions;
